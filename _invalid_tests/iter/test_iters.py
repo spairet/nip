da976911e792b9f00ffd3f9e724172b1ca3412ac
Ilya <ilya.v.vasilev@phystech.edu>
2022-07-21 03:23:21 +0300

    erased _invalid tests

--- _invalid_tests/iter/test_iters.py
+++
@@ -1,9 +0,0 @@
-from nip import nip, load, parse
-import builders
-nip(builders)
-
-# for cofig in parse("configs/complex_iter.nip"):
-#     print(cofig)
-
-for obj in load("configs/complex_iter.nip"):
-    print(obj)
--- _invalid_tests/test_wrong.py
+++
@@ -1,3 +0,0 @@
-from nip import load
-
-print(load("../tests/base_tests/raises/configs/wrong_dict_indent.nip"))
