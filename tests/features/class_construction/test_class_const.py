37edeedc359f2de1822b6e913d524d62f8c5ddc7
spairet <spairet@bk.ru>
2023-03-29 21:32:19 +0200

    test_update

--- tests/features/class_construction/test_class_const.py
+++ tests/features/class_construction/test_class_const.py
@@ -3,9 +3,13 @@ import builders
 
 
 def test_class_const():
-    result = nip.parse("features/class_construction/configs/class_const_config.nip")
-    expected_result = nip.parse("features/class_construction/configs/class_const_config.nip")
-    assert nip.construct(result) == nip.construct(expected_result)
-
-
-test_class_const()
+    result = nip.load("features/class_construction/configs/class_const_config.nip")
+    expected_result = {
+        'var': 123,
+        'main': {
+            'just': 'some_stuff',
+            'simple_class': builders.SimpleClass,
+            'def_class': builders.ClassWithDefaults
+        }
+    }
+    assert result == expected_result
