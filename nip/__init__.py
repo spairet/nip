9efd7388ab6ed13ff00a20802ecf2e3a31449be2
spairet <spairet@bk.ru>
2023-07-05 04:51:36 +0200

    new typeguard.

--- nip/__init__.py
+++ nip/__init__.py
@@ -1,3 +1,6 @@
 from .constructor import nip, wrap_module
 from .convertor import pin
 from .main import parse, construct, load, dump, dumps, convert, run
+from .parser import Parser
+from .constructor import Constructor
+from .non_seq_constructor import NonSequentialConstructor
